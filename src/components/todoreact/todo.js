d11f40dd690a18f773c9ece97e7f335376096c48
virali-b-patel <virali678@gmail.com>
2023-07-06 13:19:44 +0530

    ToDo-React-App

--- src/components/todoreact/todo.js
+++ src/components/todoreact/todo.js
@@ -1,106 +1,122 @@
-import React,{useState,useEffect} from "react";
+import React from "react";
 import "./style.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
-const getLocalData=()=>{
-  const lists=localStorage.getItem("mytodolist")
+const getLocalData = () => {
+  const lists = localStorage.getItem("mytodolist");
 
-  if(lists){
-    return JSON.parse(lists)
-  }else{
-   return[]
+  if (lists) {
+    return JSON.parse(lists);
+  } else {
+    return [];
   }
-}
+};
 
 const Todo = () => {
-  const [inputdata,setInputdata]=useState("")
-  const [items,setItems]=useState(getLocalData())
-  const [isEditItem,setIsEditItem]=useState("")
-  const [toggleButton,setToggleButton]=useState(false)
+  const [inputData, setInputData] = useState("");
+  const [items, setItems] = useState(getLocalData());
+  const [isEditItem, setIsEditItem] = useState("");
+  const [toggleButton, setToggleButton] = useState(false);
 
-const addItem=()=>{
-  if(!inputdata){
-    alert("Fill the data")
-  }else if(inputdata && toggleButton){
-    setItems(
-      items.map((element)=>{
-        if(element.id===isEditItem){
-          return {...element,name:inputdata}
-        }
-        return element
-      })
-    )
-    setInputdata("")
-    setIsEditItem(null)
-    setToggleButton(false)
-  } else{
-  const newInputData={
-    id:new Date().getTime().toString(),
-    name:inputdata,
-  }
-  setItems([...items,newInputData])
-  setInputdata("")
-}
-}
+  const addItem = () => {
+    if (!inputData) {
+      alert("Fill the data");
+    } else if (inputData && toggleButton) {
+      setItems(
+        items.map((element) => {
+          if (element.id === isEditItem) {
+            return { ...element, name: inputData };
+          } else {
+            return element;
+          }
+        })
+      );
+      setInputData([]);
+      setIsEditItem("");
+      setToggleButton(false);
+    } else {
+      const newInputData = {
+        id: new Date().getTime().toString(),
+        name: inputData,
+      };
+      setItems([...items, newInputData]);
+      setInputData("");
+    }
+  };
 
+  const editItem = (id) => {
+    const editItem = items.find((element) => {
+      return element.id === id;
+    });
+    setInputData(editItem.name);
+    setIsEditItem(id);
+    setToggleButton(true);
+  };
 
-const editItem=(index)=>{
-  const Item_todo_edited=items.find((element)=>{
-    return element.id===index
-  })
-  setInputdata(Item_todo_edited.name)
-  setIsEditItem(index)
-  setToggleButton(true)
-}
+  const deleteItem = (id) => {
+    const deleteItem = items.filter((element) => {
+      return element.id !== id;
+    });
+    setItems(deleteItem);
+  };
 
-const deleteItem=(index)=>{
-  const updatedItems=items.filter((element)=>{
-    return element.id!=index
-  })
-  setItems(updatedItems)
-}
+  const removeAll = () => {
+    setItems([]);
+  };
 
-const removeAll=()=>{
-  setItems([])
-}
+  useEffect(() => {
+    localStorage.setItem("mytodolist", JSON.stringify(items));
+  }, [items]);
 
-useEffect(()=>{
-localStorage.setItem("mytodolist",JSON.stringify(items))
-},[items])
-
- 
   return (
     <>
       <div className="main-div">
         <div className="child-div">
           <figure>
             <img src="./image/todo.svg" alt="todologo" />
-            <figcaption>Add Your List Here</figcaption>
+            <figcaption>Add Your List Here ✌️</figcaption>
           </figure>
           <div className="addItems">
-            <input type="text" placeholder="Add Item" className="form-control" value={inputdata} onChange={e=>setInputdata(e.target.value)}/>
-            {
-              toggleButton?(<i className="fa fa-edit add-btn" onClick={addItem}></i>):(<i className="fa fa-plus add-btn" onClick={addItem}></i>)
-            }
-            <i className="fa fa-plus add-btn" onClick={addItem}></i>
+            <input
+              className="form-control"
+              type="text"
+              placeholder="✍️ Add Item"
+              value={inputData}
+              onChange={(e) => setInputData(e.target.value)}
+            />
+            {toggleButton ? (
+              <i className="far fa-edit add-btn" onClick={addItem}></i>
+            ) : (
+              <i className="fa fa-plus add-btn" onClick={addItem}></i>
+            )}
           </div>
           <div className="showItems">
-            {
-              items.map((element)=>{
-                return ( 
-                  <div className="eachItem" key={element.id}>
-                    <h3>{element.name}</h3>
-                    <div className="todo-btn">
-                      <i className="fa fa-plus add-btn" onClick={()=>editItem(element.id)}></i>
-                      <i className="fa fa-plus add-btn" onClick={()=>deleteItem(element.id)}></i>
+            {items.map((element) => {
+              return (
+                <div className="eachItem" key={element.id}>
+                  <h3>{element.name}</h3>
+                  <div className="todo-btn">
+                    <i
+                      className="far fa-edit add-btn"
+                      onClick={() => editItem(element.id)}
+                    ></i>
+                    <i
+                      className="far fa-trash-alt add-btn"
+                      onClick={() => deleteItem(element.id)}
+                    ></i>
                   </div>
                 </div>
-              )
-              })
-            }
+              );
+            })}
           </div>
           <div className="showItems">
-            <button className="btn effect04" data-sm-link-text="Remove All" onClick={removeAll}>
-             <span>CHECK LIST</span> 
+            <button
+              className="btn effect04"
+              data-sm-link-text="REMOVE ALL"
+              onClick={removeAll}
+            >
+              <span>CHECK LIST</span>
             </button>
           </div>
         </div>
